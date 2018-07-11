d1505629f296ecccdb12b02e6dd6cc71f5ea5435
Takuma Nishino <takuuma.717@gmail.com>
2018-07-12 00:46:16 +0900

    1-4

--- ch1/1-4/main.go
+++ ch1/1-4/main.go
@@ -1,3 +1,4 @@
+// go run main.go test1.txt test2.txt
 package main
 
 import (
@@ -7,10 +8,11 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	var counts = map[string]int{}
+	var fileNames = map[string]map[string]string{}
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, fileNames)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,20 +20,28 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, fileNames)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			keys := []string{}
+			for k := range fileNames[line] {
+				keys = append(keys, k)
+			}
+			fmt.Printf("%d\t%v\t%s\n", n, keys, line)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, fileNames map[string]map[string]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		if _, v := fileNames[input.Text()]; !v {
+			fileNames[input.Text()] = map[string]string{}
+		}
+		fileNames[input.Text()][f.Name()] = ""
 	}
 }
