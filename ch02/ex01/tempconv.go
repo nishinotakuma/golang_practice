2e5788cfa088a27290c46559e6a4749fdb788c4b
nishinotakuma <takuuma_717@r.recruit.co.jp>
2018-07-12 15:08:15 +0900

    2-1

--- ch02/ex01/tempconv.go
+++ ch02/ex01/tempconv.go
@@ -15,3 +15,12 @@ const (
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// Kelvin to Celsius
+func KToC(k Kelvin) Celsius { return Celsius(k) + AbsoluteZeroC }
